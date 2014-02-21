bb6a8c2d48a3becd9dfff7f9af9085597bbda60f
Clément Gamé <quotekio@gmail.com>
2014-02-21 09:15:12 +0100

    [BUGFIX] (core) Fix for the Backtest results JSON encoding.

--- src/lib/results.h
+++ src/lib/results.h
@@ -28,16 +28,17 @@ class snappos {
     snappos() {}
     string json_encode() {
 
-      string res = "{";
-      res += "\"name\": " + name + ",\n";
-      res += "\"open\": " + float2string(open) + ",\n";
-      res += "\"close\": " + float2string(close) + ",\n";
-      res += "\"pnl\": " + float2string(pnl) + ",\n";
-      res += "\"opentime\": " + int2string(opentime) + ",\n";
-      res += "\"closetime\": " + int2string(closetime) + ",\n";
+      stringstream ss;
+      ss << "{";
+      ss << "\"name\": \"" << name << "\",\n";
+      ss << "\"open\": " << open << ",\n";
+      ss << "\"close\": " << close << ",\n";
+      ss << "\"pnl\": " << pnl << ",\n";
+      ss << "\"opentime\": " << opentime << ",\n";
+      ss << "\"closetime\": " << closetime << "\n";
+      ss << "}";
 
-      res += "}";
-      return res;
+      return ss.str();
 
     }
 
@@ -54,14 +55,15 @@ class assetstats {
     assetstats() {}
 
     string json_encode() {
-      string res = "{";
-      res += "\t\"name\": " + name + ",\n";
-      res += "\t\"deviation\": " + float2string(deviation) + ",\n";
-      res += "\t\"variation\": " + float2string(variation) + ",\n";
-      res += "\t\"highest\": " + float2string(highest) + ",\n";
-      res += "\t\"lowest\": " + float2string(lowest) + ",\n";
-      res+= "}";
-      return res;
+      stringstream ss;
+      ss << "{";
+      ss << "\t\"name\": \"" << name << "\",\n";
+      ss << "\t\"deviation\": " << deviation << ",\n";
+      ss << "\t\"variation\": " << variation << ",\n";
+      ss << "\t\"highest\": " << highest << ",\n";
+      ss << "\t\"lowest\": " << lowest << "\n";
+      ss << "}";
+      return ss.str();
     }
 
 
@@ -86,29 +88,34 @@ class adamresult {
 
   	string json_encode() {
 
-      string res = "{";
-      res += "\"start\": " + int2string(start) + ",\n";
-      res += "\"stop\": " + int2string(stop) + ",\n";
-      res += "\"from\": " + int2string(from) + ",\n";
-      res += "\"to\": " + int2string(to) + ",\n";
-      res += "\"pnl\": " + float2string(pnl) + ",\n";
-      res += "\"remainingpos\": " + int2string(remainingpos) + ",\n";
+      stringstream ss;
 
-      res+="\"positions\": [";
+      ss << "{";
+      ss <<  "\"start\": " << start << ",\n";
+      ss <<  "\"stop\": " << stop << ",\n";
+      ss <<  "\"from\": " << from << ",\n";
+      ss <<  "\"to\": " << to << ",\n";
+      ss <<  "\"pnl\": " << pnl << ",\n";
+      ss <<  "\"remainingpos\": " << remainingpos << ",\n";
+
+      ss << "\"positions\": [";
       for(int i=0;i<positions.size();i++) {
-        res += positions[i].json_encode() + ",\n";
+        ss <<  positions[i].json_encode();
+        if (i< positions.size()-1) ss << ",\n";
+        else ss << "\n";
       }
-      res +="],\n";
+      ss << "],\n";
 
-      res+="\"astats\": [";
+      ss << "\"astats\": [";
       for(int i=0;i<astats.size();i++) {
-        res += astats[i]->json_encode() + ",\n";
+        ss <<  astats[i]->json_encode();
+        if (i< astats.size()-1) ss << ",\n";
+        else ss << "\n";
       }
-      res +="]";
+      ss << "]";
 
-      
-      res += "}";
-      return res;
+      ss <<  "}";
+      return ss.str();
 
   	}
     
