47aba92a60a2142f04c38464c6fa0ddf24f832ec
Clément Gamé <quotekio@gmail.com>
2014-02-24 03:25:16 +0100

    (core) started to split backtest functionalities.

---
+++ src/lib/btengine.h
@@ -0,0 +1,13 @@
+
+/*
+btEngine class defines the behaviour of adam while in backtest mode.
+*/
+class btEngine {
+
+  public:
+    btEngine();
+    int loadDump();
+    int process();
+  private:
+
+};
