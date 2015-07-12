ac3fb9476c654517f5007f904fd7123201ac5ffc
Clément Gamé <quotekio@gmail.com>
2015-07-12 17:17:35 +0200

    COntinued work on c++ strats, what a pain in the A$$ !!!

--- src/lib/strategy.hpp
+++ src/lib/strategy.hpp
@@ -1,5 +1,4 @@
 #include <regex>
-#include <thread>
 
 /**
  * strategy class is one of the most important of quotek adam bot.
@@ -21,29 +20,23 @@ class strategy {
     ~strategy();
 
     /**
-     * run() method handles the execution of the strat.
-     */
-    std::thread* run();
-
-    /**
      * initialize() is a method meant to set the initial state of a strategy.
      * This method is executed only once, before the first evaluation. 
      */
-    virtual int initialize();
+    virtual int initialize() { return 0; }
 
     /**
      * evaluate() method is the very core of the the strategy, this is where 
      * the user's algorithm gets executed, at a pace of once per value tick.
      */
-    virtual void evaluate();
+    virtual void evaluate() {}
 
     /**
      * tradelife() is here to manage the open positions and decide when to close them, 
      *  or change the ticket (stops, limite, ticket size..) 
      */
-    virtual void tradelife();
+    virtual void tradelife()  {}
 
-  private:
     vector<strategy*> substrats;
 
     /** state is a variable meant to keep track of the state of the algorithm. it's up to the user to set it accordingly. */
@@ -54,5 +47,9 @@ class strategy {
 
     /** spread stores the current spread of an asset provided by the broker. */
     float spread;
+
+    /** Asset name stores the asset for which the strategy instance is running */
+    std::string asset_name;
+
 
 };
