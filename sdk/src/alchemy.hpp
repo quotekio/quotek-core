8aeef8f2d4f58f79a9f360818dc75405cdd565ee
Clément Gamé <quotekio@gmail.com>
2015-07-29 17:35:38 +0200

    Started definition of alchemy int.

---
+++ sdk/src/alchemy.hpp
@@ -0,0 +1,30 @@
+#ifndef ALCHEMY_HPP
+#define ALCHEMY_HPP
+
+namespace quotek {
+
+
+  /** ml namespace in quotek SDK regroups all the objects and methods useful to implement 
+   *  Machine Learning/Deep Learning Algorithms in your strategies */
+  namespace ml {
+
+    class alchemy {
+
+
+      public:
+
+        alchemy();
+        ~alchemy();
+
+        std::string api_key;
+        
+    };
+    
+
+
+  }
+
+}
+
+
+#endif
