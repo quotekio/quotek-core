5291941e68103911afbe19d3c8278d7e0a7da3be
Clément Gamé <quotekio@gmail.com>
2015-09-16 22:00:14 +0000

    modified batch installer for windows.

--- sdk/release/installer_win64.bat
+++ sdk/release/installer_win64.bat
@@ -3,11 +3,14 @@
 echo "Quotek SDK 2.0.0 Installer"
 echo "=========================="
 
-mkdir C:\Program Files\quotek\sdk
+mkdir "C:\Program Files\quotek\sdk"
 
-xcopy doc C:\Program Files\quotek\sdk\doc /e /y
-xcopy include C:\Program Files\quotek\sdk\include /e /y
-xcopy lib\win64 C:\Program Files\quotek\sdk\lib /e /y
+xcopy doc "C:\Program Files\quotek\sdk\doc\" /e /y
+xcopy include "C:\Program Files\quotek\sdk\include\" /e /y
+xcopy lib\win64 "C:\Program Files\quotek\sdk\lib\" /e /y
 
 copy lib\win64\libcurl.dll C:\Windows\System\
+
+echo "Quotek SDK installed succesfully, press any key to continue.."
+pause
 
