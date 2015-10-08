a3a528d8e35ef4bd928b1a1cfd994a2e1e06153a
Clément Gamé <quotekio@gmail.com>
2015-10-08 18:28:15 +0000

    Windows bugfix for quotek.hpp, wrong include order.

--- sdk/src/quotek.hpp
+++ sdk/src/quotek.hpp
@@ -7,6 +7,10 @@ http://www.quotek.io
 #ifndef QUOTEK_HPP
 #define QUOTEK_HPP
 
+#include <algorithm>
+#include "record.hpp"
+#include "quant.hpp"
+
 #include "datasources/csv.hpp"
 #include "datasources/rss.hpp"
 #include "datasources/twitter.hpp"
@@ -18,10 +22,8 @@ http://www.quotek.io
 #include "constants.hpp"
 #include "core.hpp"
 #include "generators.hpp"
-#include "quant.hpp"
 #include "http.hpp"
 #include "option.hpp"
-#include "record.hpp"
 #include "strategy.hpp"
 #include "techanalysis.hpp"
 #include "timeutils.hpp"
