54c00fc23a4ed4e8cf0d55da98af3398f6be4cda
Clément Gamé <quotekio@gmail.com>
2015-10-13 03:46:49 +0200

    Fixed csv test.

--- sdk/tests/src/test_csv.cpp
+++ sdk/tests/src/test_csv.cpp
@@ -14,7 +14,7 @@ http://www.quotek.io
 void test_import_records() {
 
   quotek::datasource::csv c1("file://fixtures/test1.csv",';');
-  std::vector<quotek::data::record> recs = c1.import_records(10,2);
+  quotek::data::records recs = c1.import_records(10,2);
 
   assert( recs.size() == 10 );
   assert( recs[0].value == 4000 );
@@ -25,7 +25,7 @@ void test_import_records_with_timestamp() {
 
   quotek::datasource::csv c1("file://fixtures/test1.csv",';');
   c1.setTimeColumn(0,"%Y-%m-%d %H:%M:%S");
-  std::vector<quotek::data::record> recs = c1.import_records(10,2);
+  quotek::data::records recs = c1.import_records(10,2);
 
   assert( recs.size() == 10 );
   assert( recs[0].value == 4000 );
@@ -37,7 +37,7 @@ void test_import_records_with_filters() {
 
   quotek::datasource::csv c1("file://fixtures/test1.csv",';');
   c1.addFilter("(.*)cac(.*)");
-  std::vector<quotek::data::record> recs = c1.import_records(10,2);
+  quotek::data::records recs = c1.import_records(10,2);
  
   assert( recs.size() == 5 );
   assert( recs[0].value == 4000 );
