3b3ed2ee06b2230b55d503149ec045900c886ee8
Clément Gamé <quotekio@gmail.com>
2016-03-26 00:49:05 +0100

    [SDK] Removal of marray since we will integrate eigen matrixes.

---
+++ sdk/src/ml.cpp
@@ -0,0 +1,14 @@
+/*
+Quotek Strategies SDK 2.3
+Copyright 2013-2016 Quotek SAS
+*/
+
+#include "ml.hpp"
+
+namespace quotek {
+
+  namespace ml {
+    
+  }
+}
+
---
+++ sdk/src/ml.hpp
@@ -0,0 +1,31 @@
+/*
+Quotek Strategies SDK 2.3
+Copyright 2013-2016 Quotek SAS
+*/
+
+#ifndef ML_HPP
+#define ML_HPP
+
+#include <algorithm>
+#include <iomanip>
+#include <string>
+#include <cstring>
+#include <sstream>
+#include <vector>
+#include <ctime>
+#include <time.h>
+
+
+namespace quotek {
+
+  /**
+   * The ml namespace contains All the functions related to Machine Learning. 
+   * If you want to perform linear/logistic regressions on data, create neural nets 
+   * or anything else ML-related, then the functions you need are here. */
+
+  namespace ml {
+    
+  }
+}
+
+#endif
