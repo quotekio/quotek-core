2ab0298523f251c27caeff8a5e6063b6efafaecf
Clément Gamé <quotekio@gmail.com>
2016-04-17 12:12:36 +0200

    [SDK] New test for ML linear regression

---
+++ sdk/tests/src/test_linear_regression.cpp
@@ -0,0 +1,31 @@
+#include <quotek/linearregression.hpp>
+
+void test_linear_regression() {
+
+  quotek::ml::dataset X;
+  quotek::ml::dvector y;
+
+  quotek::ml::dataset X_; 
+
+  X << 1 , 2, 3, 4 , 5;
+  y << 
+
+
+
+  quotek::ml::linearRegression l1();
+  l1.train(X,y);
+
+  l1.predict()
+
+
+  assert(    );
+
+
+
+}
+
+
+int main() {
+
+  test_linear_regression();
+}
