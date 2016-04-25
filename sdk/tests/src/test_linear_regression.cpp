033109feb9de4943027bc3f1677944a584ff6357
Clément Gamé <quotekio@gmail.com>
2016-04-25 18:02:27 +0200

    [SDK][TESTS] Continiued work on lreg tests.

--- sdk/tests/src/test_linear_regression.cpp
+++ sdk/tests/src/test_linear_regression.cpp
@@ -7,18 +7,19 @@ void test_linear_regression() {
 
   quotek::ml::dataset X_; 
 
-  X << 1 , 2, 3, 4 , 5;
-  y << 
+  X =  MatrixXd(1, 5);
+  y =  VectorXd(5);
+  
+  X << 1 , 2, 3, 4, 5;
+  y << 1,  2, 3, 4, 5;
 
+  quotek::ml::linearRegression l1;
 
-
-  quotek::ml::linearRegression l1();
   l1.train(X,y);
 
-  l1.predict()
 
-
-  assert(    );
+  //l1.predict()
+  //assert(    );
 
 
 
